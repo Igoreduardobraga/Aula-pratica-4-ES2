efd0715e6995c7753836bb03a30422e7c7eafca1
igoreduardobraga <igoredubraga@gmail.com>
2024-12-09 17:47:59 -0300

    adicionando arquivo de actions e o de teste

--- src/gerenciador.py
+++ src/gerenciador.py
@@ -1,3 +1,35 @@
+class Tarefa:
+    def __init__(self, titulo, descricao):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.concluida = False
 
-    
+    def marcar_concluida(self):
+        self.concluida = True
 
+    def __repr__(self):
+        status = "✅" if self.concluida else "❌"
+        return f"{status}{self.titulo}"
+
+
+class Gerenciador:
+    def __init__(self):
+        self.tarefas = []
+
+    def adicionar_tarefa(self, titulo, descricao):
+        tarefa_nova = Tarefa(self, titulo, descricao)
+        self.tarefas.append(Tarefa(self, titulo, descricao))
+        return tarefa_nova
+
+    def listar_tarefas_pendentes(self):
+        return [t for t in self.tarefas if not t.concluida]
+
+    def listar_tarefas_concluidas(self):
+        return [t for t in self.tarefas if t.concluida]
+
+    def marcar_tarefa_concluida(self, titulo):
+        for tarefa in self.tarefas:
+            if tarefa.titulo == titulo:
+                tarefa.marcar_concluida()
+                return tarefa
+        return None
