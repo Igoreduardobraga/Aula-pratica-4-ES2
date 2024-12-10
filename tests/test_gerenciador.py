439bf7083f2d1e89fed8fee1acd3bc8d3bede7a3
igoreduardobraga <igoredubraga@gmail.com>
2024-12-09 21:08:01 -0300

    adicionando mais testes

--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -1,6 +1,16 @@
-import pytest
 from src.gerenciador_tarefas import GerenciadorTarefas
+from src.gerenciador_tarefas import Tarefa
 
+def test_cirar_tarefa():
+    tarefa = Tarefa("Tarefa 1", "Ir para a academia")
+    assert tarefa.titulo == "Tarefa 1"
+    assert tarefa.concluida == False
+    assert tarefa.descricao == "Ir para a academia"
+
+def test_marcar_tarefa():
+    tarefa = Tarefa("Tarefa 1", "Ir ao mercado")
+    tarefa.marcar_concluida()
+    assert tarefa.concluida == True
 
 def test_adicionar_tarefa():
     g = GerenciadorTarefas()
@@ -37,10 +47,17 @@ def test_listar_tarefas_concluidas():
 
 def test_marcar_tarefa_concluida():
     g = GerenciadorTarefas()
-    t1 = g.adicionar_tarefa("Tarefa 1", "Ir ao mercado")
-    t2 = g.adicionar_tarefa("Tarefa 2", "Comprar pão")
-    t3 = g.adicionar_tarefa("Tarefa 3", "Comprar queijo")
-    t1.marcar_concluida()
-    t2.marcar_concluida()
-    t3.marcar_concluida()
+    g.adicionar_tarefa("Tarefa 1", "Ir ao mercado")
+    g.adicionar_tarefa("Tarefa 2", "Comprar pão")
+    g.adicionar_tarefa("Tarefa 3", "Comprar queijo")
+    t1 = g.marcar_tarefa_concluida("Tarefa 1")
+    t2 = g.marcar_tarefa_concluida("Tarefa 2")
+    t3 = g.marcar_tarefa_concluida("Tarefa 3")
     assert all([t1.concluida & t2.concluida & t3.concluida])
+
+def test_marcar_tarefa_inexistente():
+    g = GerenciadorTarefas()
+    g.adicionar_tarefa("Tarefa 1", "Ir ao mercado")
+    g.adicionar_tarefa("Tarefa 2", "Comprar pão")
+    g.adicionar_tarefa("Tarefa 3", "Comprar queijo")
+    assert g.marcar_tarefa_concluida("Jogar futebol") == None
